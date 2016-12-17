718406806abbd28219dc3189c8e8747189bcc873
Michelle Lee <michelle.lee@wattpad.com>
2016-12-17 13:53:41 -0500

    Node.js scraper reads in CSV of IDs and batch outputs JSON

--- epa-eis-collection/2-scraper/index.js
+++ epa-eis-collection/2-scraper/index.js
@@ -1,6 +1,10 @@
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
 var fs = require( 'fs' );
+var Converter = require( "csvtojson" ).Converter;
+var converter = new Converter( {} );
+var CSV_FILENAME = "eis-sample-page-1.csv"
+
 var pageData = {
   metaData: {
     'EIS Title': '',
@@ -22,28 +26,47 @@ var pageData = {
   pdfLinks: []
 };
 
-request( 'https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/details?eisId=222951', function( err, res, html ) {
-  if ( !err && res.statusCode == 200 ) {
-    var $ = cheerio.load( html );
-    $( '.form-item' ).each( function( i, element ) {
-      var formItem = $( element );
-      var text = formItem.text().trim().replace( /\s\s+/g, ' ' );
-      for ( var prop in pageData.metaData ) {
-        if ( text.indexOf( prop ) !== -1 ) {
-          pageData.metaData[ prop ] = text.replace( prop, '' );
-        }
+converter.fromFile( CSV_FILENAME, function( err, result ) {
+  var eidIdArray = result.map( function( obj ) {
+    return obj.eid_id;
+  } );
+
+  eidIdArray.forEach( function( id ) {
+    request( 'https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/details?eisId=' + id, function( err, res, html ) {
+      if ( !err && res.statusCode == 200 ) {
+        console.log( "Scraping page", id, "..." );
+        var $ = cheerio.load( html );
+        $( '.form-item' ).each( function( i, element ) {
+          var formItem = $( element );
+          var text = formItem.text().trim().replace( /\s\s+/g, ' ' );
+          for ( var prop in pageData.metaData ) {
+            if ( text.indexOf( prop ) !== -1 ) {
+              pageData.metaData[ prop ] = text.replace( prop, '' );
+            }
+          }
+        } );
+        $( '.node.node-page div:last-child div:last-child p a' ).each( function( i, element ) {
+          var linkElement = $( element );
+          var linkHref = 'https://cdxnodengn.epa.gov' + linkElement.attr( 'href' );
+          var linkText = linkElement.text();
+          pageData.pdfLinks.push( {
+            'pdf-link': linkHref,
+            'pdf-filename': linkText
+          } );
+        } );
+
+        fs.writeFile( 'output/page-metadata-' + id + '.json', JSON.stringify( pageData ), 'utf8', function( err ) {
+          if ( err ) {
+            console.error( err );
+            return;
+          } else {
+            console.log( "Finished scraping page", id );
+          }
+        } );
+      } else {
+        console.error( "Sorry, you encountered an error. Is this link valid?", 'https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/details?eisId=' + id );
       }
     } );
-    $( '.node.node-page div:last-child div:last-child p a' ).each( function( i, element ) {
-      var linkElement = $( element );
-      var linkHref = 'https://cdxnodengn.epa.gov' + linkElement.attr( 'href' );
-      var linkText = linkElement.text();
-      pageData.pdfLinks.push( {
-        'pdf-link': linkHref,
-        'pdf-filename': linkText
-      } );
-    } );
 
-    fs.writeFileSync( 'page-metadata.json', JSON.stringify( pageData ), 'utf8' );
-  }
+  } );
 } );
